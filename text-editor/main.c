c614971299b3db4cf198a92486c21e18b48b3f41
DylanPrinsloo <dylanprinsloo@icloud.com>
2024-08-24 17:04:30 +0200

    added execute

--- text-editor/main.c
+++ text-editor/main.c
@@ -1,50 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
-
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-struct termios orig_termios;
-
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
-
-void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
-
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
-    raw.c_iflag &= ~(IXON);
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-void editorProcessKeypress() {
-    char c = '\0';
-    read(STDIN_FILENO, &c, 1);
-
-    switch (c) {
-        case CTRL_KEY('q'):
-            exit(0);
-            break;
-    }
-}
-
-void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
 
 int main() {
-    enableRawMode();
-
-    while (1) {
-        editorRefreshScreen();
-        editorProcessKeypress();
-    }
-
+    printf("Hello, World!\n");
     return 0;
 }
