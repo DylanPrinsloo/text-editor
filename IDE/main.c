85ed8f6a168e24becf2a1664ee92ef77712f17e3
Trckmazz <dylanprinsloo@icloud.com>
2024-08-31 16:19:13 +0200

    added BRKINT, INPCK, ISTRIP

--- IDE/main.c
+++ IDE/main.c
@@ -4,6 +4,13 @@
 #include <ctype.h>
 #include <stdio.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+/*** terminal ***/
+/*** init ***/
+
 Struct termios orig_termios;
 
 void disableRawMode() {
@@ -14,8 +21,11 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     Struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw/c_oflag &= ~(OPOST);
+     raw/c_oflag &= ~(CS8); // add CS ------------!
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    // Ctrl-V == 22 byte && Ctrl-0 == 15 byte
 
     tcsettar(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,10 +36,11 @@ int main() {
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c =! 'q') {
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ( '%c' )\n", c, c);
+            printf("%d ( '%c' )\r\n", c, c); // write in full to start new line!
         }
+        if (c == CTRL_KEY('q')) break;
     };
     return 0;
 }
