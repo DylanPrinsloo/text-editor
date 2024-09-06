1b56886cab3ed29705c70a8e424648d113a73399
Trckmazz <dylanprinsloo@icloud.com>
2024-09-06 14:45:42 +0200

    commit

--- IDE/main.c
+++ IDE/main.c
@@ -1,23 +1,29 @@
+/*** Includes ***/
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 /*** defines ***/
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-/*** data ***/
-/*** terminal ***/
-/*** init ***/
+// #define CTRL_KEY(k) ((k) & 0x1f)
 
 Struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) ==-1) die("tcsettar");
 }
 void enableRawMode() {
-    tcgettar(STDIN_FILENO, &orig_termiso);
+    if (tcgettar(STDIN_FILENO, &orig_termiso) ==-1) die("tcgettar");
     atexit(disableRawMode);
 
     Struct termios raw = orig_termios;
@@ -26,23 +32,26 @@ void enableRawMode() {
      raw/c_oflag &= ~(CS8); // add CS ------------!
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     // Ctrl-V == 22 byte && Ctrl-0 == 15 byte
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsettar(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsettar(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c =! 'q') {
+    while (1) {
+        char c = '\0':
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ( '%c' )\r\n", c, c); // write in full to start new line!
         }
-        if (c == CTRL_KEY('q')) break;
-    };
+        if (c =='q') break;
+    }
     return 0;
 }
-
-// Display key presses
